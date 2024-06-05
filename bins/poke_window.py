06155e519290cb658a5ea04b3004b5ff4ba5a356
gostnort <gostnort@hotmail.com>
2024-06-05 13:43:54 -0700

    complete to get the special items

--- bins/poke_window.py
+++ bins/poke_window.py
@@ -2,21 +2,18 @@ import json
 import threading
 import keyboard_simulate
 import argparse
-from pynput.keyboard import Controller as key_controller
 from handle_sy import SY
 from handle_se import SE
 from handle_pd import PD
 import functions
+import re
+from handle_bnd import BND
 '''
 input:string,string,float,bol
 input example: 'c:\\users','983/984/01JUN/02JUN/PEK',0.7,True
 '''
-class RequestData():
-    COMMAND_END_MARK = '\n===END===\n'
-    ARRIVAL_END_MARK = '\n===ARRIVAL_END===\n'
-    DEPARTURE_END_MARK = '\n===DEPARTURE_END===\n'
-
-    def __init__(self, JsonFolder, FlightInfo, CommandPending, Debug=False):
+class GetBriefingCommand():
+    def __init__(self, JsonFolder, FlightInfo):
         super().__init__()
         with open(JsonFolder + r'\briefing_command.json','r') as file:
             json_structure = json.load(file)
@@ -35,10 +32,33 @@ class RequestData():
             "departure_date":departure_flight_date,
             "ac_reg":""
         }
-        self.commands=self.__fill_placeholders(json_structure,values)
+        self.Commands = self.__fill_placeholders(json_structure,values)
+    
+        # Function to replace placeholders
+    def __fill_placeholders(self, obj, values):
+        if isinstance(obj, str):
+            # Replace placeholders in the string
+            for key, value in values.items():
+                obj = obj.replace(f"${{{key}}}", value)
+            return obj
+        elif isinstance(obj, list):
+            # Recursively handle lists
+            return [self.__fill_placeholders(item, values) for item in obj]
+        elif isinstance(obj, dict):
+            # Recursively handle dictionaries
+            return {key: self.__fill_placeholders(value, values) for key, value in obj.items()}
+        return obj
+
+class RequestData():
+    COMMAND_END_MARK = '\n===END===\n'
+    ARRIVAL_END_MARK = '\n===ARRIVAL_END===\n'
+    DEPARTURE_END_MARK = '\n===DEPARTURE_END===\n'
+
+    def __init__(self,BriefCommands,CommandPending, Debug=False):
+        self.commands=BriefCommands
         self.bol_debug = Debug
         self.command_pending_time=CommandPending
-        self.run()
+        #self.run()
 
     def run(self):
         event1=threading.Event()
@@ -58,21 +78,6 @@ class RequestData():
         print(listener_loop.click_position)
         listener_loop.join()
         listener_loop.event.clear()
-        
-    # Function to replace placeholders
-    def __fill_placeholders(self, obj, values):
-        if isinstance(obj, str):
-            # Replace placeholders in the string
-            for key, value in values.items():
-                obj = obj.replace(f"${{{key}}}", value)
-            return obj
-        elif isinstance(obj, list):
-            # Recursively handle lists
-            return [self.__fill_placeholders(item, values) for item in obj]
-        elif isinstance(obj, dict):
-            # Recursively handle dictionaries
-            return {key: self.__fill_placeholders(value, values) for key, value in obj.items()}
-        return obj
 
     def __send_arrival_commands(self,listener):
         for key in self.commands['arrival_section'][0]:
@@ -156,9 +161,10 @@ class RequestData():
 class HandleData():
     END_MARK = '===END==='
     ARRIVAL_MARK='===ARRIVAL_END=== '
-    def __init__(self,FilePath,Arrival):
+    def __init__(self,BriefCommands,FilePath):
         super().__init__()
         txt_list = functions.ReadTxt2List(FilePath)
+        self.commands=BriefCommands
         self.arrival_set=[]
         self.departure_set=[]
         tmp_str = ''
@@ -175,19 +181,20 @@ class HandleData():
             else:
                 self.departure_set.append(tmp_str)
                 tmp_str = ''
-        self.arrival = Arrival
         self.arrival_leg=''
         self.arrival_seat_configuration=''
         self.arrival_ac_reg=''
         self.arrival_pax_break_down=''
         self.arrival_blocked_seats=''
+        self.special={}
+        self.comment={}
         self.__get_arrival_data() #It would clear data after processing.
         self.__get_departure_data()
 
     def __get_arrival_data(self):
         for index,command in enumerate(self.arrival_set):
             if command.find('SY:') != -1:
-                my_sy=SY(command,self.arrival)
+                my_sy=SY(command,self.commands['arrival'])
                 self.arrival_ac_reg=my_sy.ac_reg
                 self.arrival_leg=my_sy.leg
                 self.arrival_pax_break_down=my_sy.checked
@@ -196,9 +203,7 @@ class HandleData():
                 continue
             if command.find('SE:') != -1:
                 my_se=SE(command,'X')
-                print(command)
                 self.arrival_blocked_seats=my_se.combination_seats
-                print(self.arrival_blocked_seats)
                 self.arrival_set[index] = ''
                 continue
         
@@ -207,14 +212,36 @@ class HandleData():
             if command.find('PD:') != -1:
                 pd=PD()
                 list_command = functions.String2List(command)
-                pd.GetLastCount(list_command)
+                count = pd.GetLastCount(list_command)
+                key = self.__get_pd_special_key(command)
+                if count!=0:
+                    self.special[key]=count
+            if 'BND:' in command:
+                bnd=BND(command).numbers
+                if bnd != '':
+                    for key in self.commands['departure_section'][0]:
+                        value=self.commands['departure_section'][0][key]
+                        if 'BND' in value:
+                            new_key=key[8:]
+                            self.special[new_key]=bnd
+                            break
+        print(self.special)
+
+    def __get_pd_special_key(self,command):
+        for key in self.commands['departure_section'][0]:
+            if 'special_' in key:
+                input_command=str(self.commands['departure_section'][0][key]).upper()
+                pattern=re.compile(input_command[:2]+':.'+input_command[2:])
+                if pattern.search(command):
+                    return key[8:]
 
 
 def main():
     parser = argparse.ArgumentParser(description="PD start")
     parser.add_argument("--debug", action="store_true", help="Enable debug mode")
     args = parser.parse_args()
-    #RequestData(r'C:\Users\Admin\Downloads\PdStar0.2-main\resources',r'818/818/01JUN/01JUN/IAD',0.5,args.debug)
-    HandleData(r'C:\Users\gostn\OneDrive\桌面\eterm\qqqqq.txt','IAD')
+    briefing_commands=GetBriefingCommand(r'C:\Users\gostn\我的Github库\PdStar0.2\resources',r'818/818/01JUN/01JUN/IAD').Commands
+    #RequestData(briefing_commands, 0.5,args.debug)
+    HandleData(briefing_commands,r'C:\Users\gostn\OneDrive\桌面\eterm\qqqqq.txt')
 if __name__ == "__main__":
     main()
